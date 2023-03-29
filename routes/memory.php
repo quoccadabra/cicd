a8ea3f5209e1912434b3d41d92668dbad404af0c
Quoc Nguyen <quoc.nguyen@cadabra.jp>
2023-03-29 16:19:53 +0700

    test

--- routes/memory.php
+++ routes/memory.php
@@ -1,11 +1,22 @@
 <?php
 
 use App\Http\Controllers\MemoryController;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
-Route::middleware(['guest'])->name('guest.')->prefix('guest')->group(function () {
-    Route::get('memory', MemoryController::class)->name('memory');
-});
-Route::middleware(['auth'])->name('auth.')->prefix('my')->group(function () {
-    Route::get('memory', MemoryController::class)->name('memory');
-});
+// Route::prefix(['guest'])->name('guest.')->group(function () {
+//     Route::prefix('guest', function () {
+//         Route::get('memory', MemoryController::class)->name('memory');
+//     });
+// });
+// Route::middleware(['auth'])->name('auth.')->group(function () {
+//     Route::prefix('auth', function () {
+//         Route::get('memory', MemoryController::class)->name('memory');
+//     });
+// });
+Route::prefix('guest')->prefix('guest')->group(function () {
+    Route::resource('memory', MemoryController::class);
+})->middleware('guest');
+
+Route::prefix('auth')->prefix('auth')->group(function () {
+    Route::resource('memory', MemoryController::class);
+})->middleware('auth');
